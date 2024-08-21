e20383738b174b37ebb40f7be0ba069f188e2807
Haeahmed <haedarahasan69@gmail.com>
2024-08-21 17:26:41 +0300

    final

--- src/components/Container/Container.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import './Container.css'
-export default function Container({children}) {
-  return (
-    <div className='container'>
-      {children}
-    </div>
-  )
-}
